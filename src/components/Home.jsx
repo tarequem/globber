4787617946e4703925be7d214485dcde89c3da54
Trevor Bos <trevor.bos@outlook.com>
2022-10-06 07:40:22 -0400

    playing with colors, trying to figure out layout for home page/login page.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div>
+    <div className="bg-yellow-900">
       <div>
-        <p className="text-xl">Welcome to Globber!</p>
+        <p className="text-4xl">Welcome to Globber!</p>
         <div>
             <p className="mx-auto flex items-center">
               Please login or signup below!
@@ -14,18 +14,18 @@ const Home = () => {
               type="text"
               name="username"
               placeholder="Username"
-              className="p-2 bg-transparent border-2 border-gray-500 rounded-md text-white focus:outline-none mx-auto flex items-center"
+              className="p-2 border-2 border-emerald-500 rounded-md text-white focus:outline-none mx-auto flex items-center"
             />
             <input
               type="text"
               name="password"
               placeholder="Password"
-              className="my-4 p-2 bg-transparent border-2 border-gray-500 rounded-md text-white focus:outline-none mx-auto flex items-center"
+              className="my-4 p-2 border-2 border-emerald-500 rounded-md text-white focus:outline-none mx-auto flex items-center"
             />
-            <button className="text-white bg-gradient-to-b from-green-500 to-green-800 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
+            <button className="text-white bg-emerald-500 px-6 py-3 my-2 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
               Chat with Globber!
             </button>
-            <button className="text-white bg-gradient-to-b from-blue-500 to-blue-800 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
+            <button className="text-white bg-emerald-500 px-6 py-3 my-2 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
               Register Now!
             </button>
           </form>

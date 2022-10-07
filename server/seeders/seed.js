1a582bf8d25ebd75fd327616f87b73bce358cd41
tarequem <moatartareque@gmail.com>
2022-10-06 20:32:38 -0400

    removed unneeded friends in seed

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -20,21 +20,6 @@ db.once('open', async () => {
 
     const createdUsers = await User.collection.insertMany(userData);
 
-    //create friends
-    for (let i = 0; i < 100; i += 1) {
-        const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
-        const { _id: userId } = createdUsers.ops[randomUserIndex];
-
-        let friendId = userId;
-
-        while (friendId === userId) {
-            const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
-            friendId = createdUsers.ops[randomUserIndex];
-        }
-
-        await User.updateOne({ _id: userId }, { $addToSet: { friends: friendId } });
-    }
-
     //create globs
     let createdGlobs = [];
     for (let i = 0; i < 100; i += 1) {

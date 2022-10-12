9958ac1c82bba739e675dd1358c81ef9355f7f0f
michaelsu01@gmail.com <michaelsu01@gmail.com>
2022-10-11 20:21:57 -0400

    authlink added..

--- client/src/App.js
+++ client/src/App.js
@@ -12,10 +12,23 @@ import { setContext } from '@apollo/client/link/context';
 const httpLink = createHttpLink({
   uri: 'http://localhost:3001/graphql',
 });
+
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
+
 
 function App() {
   return (

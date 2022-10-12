551796899ff51bff48b949ef0b3b69f904e1e27d
michaelsu01@gmail.com <michaelsu01@gmail.com>
2022-10-11 20:16:22 -0400

    added apollo and graphql imports to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,30 @@
 import Home from "./components/Home";
 import Chat from "./components/Chat";
 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql',
+});
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <div>
-      <Home />
-      {/* <Chat /> */}
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+          {/* <Home /> */}
+          <Chat />
+      </div>
+    </ApolloProvider>
   );
 }
 
